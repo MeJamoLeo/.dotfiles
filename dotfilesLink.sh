f2ff3d5e7774a1d235b2907d3792202967db0206
treo <treo@pop-os.localdomain>
2022-07-12 16:46:59 -0500

    Initial commit

---
+++ dotfilesLink.sh
@@ -0,0 +1,7 @@
+ln -sf $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc
+ln -sf $HOME/.dotfiles/vim/.vimrc.maps $HOME/.vimrc.maps
+ln -sf $HOME/.dotfiles/vim/.vimrc.funcs $HOME/.vimrc.funcs
+ln -sf $HOME/.dotfiles/vim/.vimrc.plug $HOME/.vimrc.plug
+
+ln -sf $HOME/.dotfiles/xmodmap/.Xmodmap $HOME/.Xmodmap
+ln -sf $HOME/.dotfiles/xmodmap/.Xmodmap_original $HOME/.Xmodmap_original
---
+++ vim/.vimrc
@@ -0,0 +1,11 @@
+set number
+set cursorline
+set clipboard+=unnamedplus
+set autoindent
+set smartindent
+set ignorecase
+set shiftwidth=4
+
+source ~/.vimrc.plug
+source ~/.vimrc.maps
+source ~/.vimrc.funcs
