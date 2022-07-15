114c56b28eee80a166a5b4062dcb009adeb91e62
treo <treo@treo-mac.localdomain>
2022-07-15 13:35:00 -0500

    fix: case for multiple OS types and shell tpyes

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,5 +3,20 @@ ln -sf $HOME/.dotfiles/vim/.vimrc.maps $HOME/.vimrc.maps
 ln -sf $HOME/.dotfiles/vim/.vimrc.funcs $HOME/.vimrc.funcs
 ln -sf $HOME/.dotfiles/vim/.vimrc.plug $HOME/.vimrc.plug
 
-ln -sf $HOME/.dotfiles/xmodmap/.Xmodmap $HOME/.Xmodmap
-ln -sf $HOME/.dotfiles/xmodmap/.Xmodmap_original $HOME/.Xmodmap_original
+case "$OSTYPE" in
+	darwin*)
+		;;
+	linux*)
+		ln -sf $HOME/.dotfiles/xmodmap/.Xmodmap $HOME/.Xmodmap
+		ln -sf $HOME/.dotfiles/xmodmap/.Xmodmap_original $HOME/.Xmodmap_original
+		;;
+esac
+
+case "$SHELL" in
+	/bin/zsh)
+		ln -sf $HOME/.dotfiles/.zshrc $HOME/.zshrc
+		;;
+	/bin/bash)
+		ln -sf $HOME/.dotfiles/.bashrc $HOME/.bashrc
+		;;
+esac
