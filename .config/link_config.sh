bf7376ab9e4b17350cd82a5e7abf5618ddee072f
treo <treo@pop-os.localdomain>
2024-04-13 17:32:03 -0500

    updated

---
+++ .config/link_config.sh
@@ -0,0 +1,5 @@
+ln -s ~/.dotfiles/.config/alacritty ~/.config/alacritty
+ln -s ~/.dotfiles/.config/dunst ~/.config/dunst
+ln -s ~/.dotfiles/.config/i3 ~/.config/i3
+ln -s ~/.dotfiles/.config/picom ~/.config/picom
+ln -s ~/.dotfiles/.config/rofi ~/.config/rofi
